f5140ed3f9f06b938c2b90c4e00505440248c965
CodeWizardAkash <akasmajhi038@gmail.com>
2026-03-04 00:21:44 +0530

    Fixed case-sensitive import paths for Vercel build

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Banner from '../components/banner';
 import Freebook from '../components/Freebook';
