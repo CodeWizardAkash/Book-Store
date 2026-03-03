6bc4bfdf7f3b84d2dfb49834a3f748ce6994a532
CodeWizardAkash <akasmajhi038@gmail.com>
2026-03-04 00:28:20 +0530

    Fixed case-sensitive for Vercel build smae problem

--- frontend/src/courses/Courses.jsx
+++ frontend/src/courses/Courses.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import Course from "../components/course";
 import Footer from "../components/Footer"
 
