de59ec284a7a0619f64bbff667549ef0fd65ddd9
CodeWizardAkash <akasmajhi038@gmail.com>
2026-03-04 00:24:14 +0530

    Fixed case-sensitive for Vercel build

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import Banner from '../components/banner';
+import Banner from '../components/Banner';
 import Freebook from '../components/Freebook';
 function Home(){
   return (
